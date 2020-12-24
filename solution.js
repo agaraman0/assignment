15a738388cc2b0d4ff8caa23a9ba87bafe8b5d35
Aman Agarwal <agaraman0@gmail.com>
2020-12-24 14:31:59 +0530

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,12 @@
+
+bid_date = document.querySelectorAll(".lbl-licitacao font font")[9].innerHTML;
+console.log("bid_date Date Value :" , bid_date);
+
+pub = document.querySelectorAll(".lbl-licitacao font font")[1].innerHTML;
+console.log("pub Date Value :" , pub);
+
+link = document.querySelectorAll(".field .field__item a")[0].getAttribute("href");
+console.log("Links : ", link);
+
+objs = document.querySelectorAll(".views-field-nothing .field-content p font font")[0].innerHTML;
+console.log("obj Value :" , objs);
